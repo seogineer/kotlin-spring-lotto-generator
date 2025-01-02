729b8ee854eba88f2cc0e54abd49d030cdc209a6
seogineer <dgseo8981@gmail.com>
2025-01-02 14:15:32 +0900

    fix: 리액트에서 CORS 오류 해결을 위한 설정 추가

---
+++ src/main/kotlin/com/seogineer/kotlinspringlottogenerator/Config/WebConfig.kt
@@ -0,0 +1,27 @@
+package com.seogineer.kotlinspringlottogenerator.Config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        val serverIp: String = when (System.getProperty("spring.profiles.active")) {
+            "prod" -> "https://seogineer.github.io/react-lotto-generator/"
+            else -> "http://localhost:3000"
+        }
+
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins(serverIp)
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+            }
+        }
+    }
+}
