89aebfaaa9f456ec91cb5bb864bc66c76728ae0a
seogineer <dgseo8981@gmail.com>
2025-01-18 14:19:50 +0900

    fix: CORS 설정 임시 제거

--- src/main/kotlin/com/seogineer/kotlinspringlottogenerator/Config/WebConfig.kt
+++ src/main/kotlin/com/seogineer/kotlinspringlottogenerator/Config/WebConfig.kt
@@ -7,21 +7,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig {
-    @Bean
-    fun corsConfigurer(): WebMvcConfigurer {
-        val serverIp: String = when (System.getProperty("spring.profiles.active")) {
-            "prod" -> "https://seogineer.github.io"
-            else -> "http://localhost:3000"
-        }
-
-        return object : WebMvcConfigurer {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**")
-                    .allowedOrigins(serverIp)
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true)
-            }
-        }
-    }
+//    @Bean
+//    fun corsConfigurer(): WebMvcConfigurer {
+//        val serverIp: String = when (System.getProperty("spring.profiles.active")) {
+//            "prod" -> "https://seogineer.github.io"
+//            else -> "http://localhost:3000"
+//        }
+//
+//        return object : WebMvcConfigurer {
+//            override fun addCorsMappings(registry: CorsRegistry) {
+//                registry.addMapping("/**")
+//                    .allowedOrigins(serverIp)
+//                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                    .allowedHeaders("*")
+//                    .allowCredentials(true)
+//            }
+//        }
+//    }
 }
