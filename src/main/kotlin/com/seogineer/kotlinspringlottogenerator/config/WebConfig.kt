5ad3d0563159806a8dba3bcea712134952bd0524
seogineer <dgseo8981@gmail.com>
2025-01-21 21:35:35 +0900

    fix: CORS 설정 복원

--- src/main/kotlin/com/seogineer/kotlinspringlottogenerator/config/WebConfig.kt
+++ src/main/kotlin/com/seogineer/kotlinspringlottogenerator/config/WebConfig.kt
@@ -6,15 +6,19 @@ import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@Profile("dev")
 @Configuration
 class WebConfig {
     @Bean
     fun corsConfigurer(): WebMvcConfigurer {
+        val serverIp: String = when (System.getProperty("spring.profiles.active")) {
+            "prod" -> "https://seogineer.github.io"
+            else -> "localhost"
+        }
+
         return object : WebMvcConfigurer {
             override fun addCorsMappings(registry: CorsRegistry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
+                    .allowedOrigins(serverIp)
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true)
